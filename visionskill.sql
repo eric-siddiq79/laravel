a48933efc24e96ea7b01a11aa751acfeb9d60ff7
Siddikur Rahman <sm.siddik11@gmail.com>
2020-06-22 19:05:22 +0600

    added database

--- visionskill.sql
+++ visionskill.sql
@@ -23,10 +23,12 @@ DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categories` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(300) NOT NULL,
   `code` int(11) NOT NULL,
-  `userid` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `userid` int(11) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,8 +37,63 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES ('qwerqweraweadfasdf',10085,5);
+INSERT INTO `categories` VALUES (1,'eraf',98,5),(2,'eadfawe',848,5),(3,'asdfas',9495,5);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_image`
+--
+
+DROP TABLE IF EXISTS `product_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_image` (
+  `product_id` int(20) NOT NULL,
+  `image_url` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_image`
+--
+
+LOCK TABLES `product_image` WRITE;
+/*!40000 ALTER TABLE `product_image` DISABLE KEYS */;
+INSERT INTO `product_image` VALUES (4,'product-image/15927412350.jpg'),(4,'product-image/15927412351.jpg'),(5,'product-image/15927416440.png'),(5,'product-image/15927416441.jpg'),(5,'product-image/15927416442.jpg'),(6,'product-image/15927429150.webp'),(6,'product-image/15927429151.jpg'),(6,'product-image/15927429152.jpg'),(7,'product-image/15927459520.jpg'),(7,'product-image/15927459521.jpg'),(7,'product-image/15927459522.webp'),(8,'product-image/15927529850.jpg'),(9,'product-image/15927535800.jpg'),(17,'product-image/15928229360.png'),(17,'product-image/15928229361.png'),(18,'product-image/15928230450.png'),(18,'product-image/15928230451.png'),(18,'product-image/15928230452.docx'),(19,'product-image/15928230710.png'),(20,'product-image/15928231610.png'),(20,'product-image/15928231621.png'),(21,'product-image/15928231930.png'),(22,'product-image/15928232960.png'),(22,'product-image/15928232961.png'),(23,'product-image/15928233230.png'),(23,'product-image/15928233231.png'),(24,'product-image/15928275710.png'),(25,'product-image/15928276610.png'),(25,'product-image/15928276611.png');
+/*!40000 ALTER TABLE `product_image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `regular_price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `flate_price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `shortdes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cat_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `buy_price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `feature_image` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'product_placholder.jpg',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (6,'Mac Book Pro','985','919','17\"','<p>adsfasdfMamurjor!</p>','98','591','asdf','product-image/15927429152.jpg'),(8,'Siddiqur Rahman','9191','919','adfasdfMamurjor!','<p>adsfasdfMamurjor!</p>','98','591','asdf','product-image/15927529850.jpg'),(20,'Hp','9191','919','lorem Ipsum','<p>adsfasdfMamurjor!</p>','98','591','asdf','product-image/15928233231.png'),(24,'Siddiqur Rahman','9191','919','adfasdfMamurjor!','<p>adsfasdfMamurjor!</p>','98','591','asdf','product-image/15928275710.png'),(25,'Lenevo','9191','919','adfasdfMamurjor!','<p>adsfasdfMamurjor!</p>','98','591','asdf','product-image/15928276611.png');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -74,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-21 21:39:36
+-- Dump completed on 2020-06-22 19:05:06
